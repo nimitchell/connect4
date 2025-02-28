03104e6f7fccedede12144d263196eb6a1f2fc80
Nicholas <nmitchell93099@gmail.com>
2025-02-28 00:47:44 -0500

    fml

--- connect4/__main__.py
+++ connect4/__main__.py
@@ -5,4 +5,4 @@ from connect4.connect4 import fib  # random comment test
 if __name__ == "__main__":
     n = int(sys.argv[1])
     print("hello world")
-    print("hi    helloooooooo  ")
+    print("WHAT THE FUCK")
