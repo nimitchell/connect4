3ad240f1d1ed1fd5ca7f03626723e4478f40455e
Nicholas <nmitchell93099@gmail.com>
2025-03-10 18:36:24 -0400

    use mutliline strings

--- connect4/classes/State.py
+++ connect4/classes/State.py
@@ -20,4 +20,10 @@ class State:
         self.previous = previous
 
     def __str__(self):
-        return f"Players:\n{[f'{p.player_num}: {p.name}\n' for p in self.players]}Turn: {self.turn} ({self.current_player.name})\n{self.board}\nPrevious turn:{f'{self.previous.turn} ({self.previous.current_player.name if self.previous else ''})'}"
+        return (
+            f"Players:\n"
+            f"{[f'{p.player_num}: {p.name}\n' for p in self.players]}"
+            f"Turn: {self.turn} ({self.current_player.name})\n"
+            f"{self.board}\n"
+            f"Previous turn:{f'{self.previous.turn} ({self.previous.current_player.name if self.previous else ''})'}"
+        )
